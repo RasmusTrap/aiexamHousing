7a1c68de1c95191552e8fecf80540dce9e96c482
Rasmus Møller <rasm937k@stud.kea.dk>
2023-07-24 20:01:14 +0200

    formatting

--- ai - exam/multipleregression.py
+++ ai - exam/multipleregression.py
@@ -2,22 +2,12 @@ import pandas as pd
 import statsmodels.api as sm
 from sklearn.preprocessing import StandardScaler
 
-# Load the dataset
+#Data preprocessing
 data = pd.read_csv('housing.csv')
-
-# Data preprocessing
-# Drop rows with missing values
 data = data.dropna()
-
-# Split the data into features (X) and target (y)
 X = data.drop(columns=['median_house_value'])  # Exclude the target variable
-
 y = data['median_house_value']
-
-# Convert categorical variables using one-hot encoding
 X = pd.get_dummies(X, columns=['ocean_proximity'], drop_first=True)
-
-# Drop problematic columns representing ocean_proximity
 X.drop(columns=['ocean_proximity_INLAND', 'ocean_proximity_ISLAND', 'ocean_proximity_NEAR BAY', 'ocean_proximity_NEAR OCEAN'], inplace=True)
 
 # Apply Standardization to the numerical features
@@ -25,14 +15,7 @@ numerical_features = X.select_dtypes(include=['float64', 'int64']).columns
 scaler = StandardScaler()
 X[numerical_features] = scaler.fit_transform(X[numerical_features])
 
-# Add a constant term to the predictor variables (required for statsmodels)
 X = sm.add_constant(X)
-
-# Create the multiple regression model
 model = sm.OLS(y, X)
-
-# Fit the model
 results = model.fit()
-
-# Print the summary of the regression analysis
 print(results.summary())
